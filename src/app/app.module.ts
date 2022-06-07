dcf7eb23fa75ee932c7de803dc761f28113ef269
Jorge Francisco <Jorge7.9@hotmail.com>
2022-06-06 21:05:18 -0300

    Rotas: Criando um módulo de rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
     CursoDetalheComponent,
     CursoNaoEncontradoComponent,
   ],
-  imports: [BrowserModule, routing, NgbModule],
+  imports: [BrowserModule, NgbModule, AppRoutingModule],
   providers: [CursosService],
   bootstrap: [AppComponent],
 })
